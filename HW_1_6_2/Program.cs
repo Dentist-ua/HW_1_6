dcd4976078eac94268b4ac8bd2f2961228c19560
Dmytro <zhorov.dmytro@gmail.com>
2023-11-02 17:55:26 +0200

    3test

--- HW_1_6_2/Program.cs
+++ HW_1_6_2/Program.cs
@@ -22,6 +22,8 @@ namespace HW_1_6_2
             var inputB = Console.ReadLine();
             int.TryParse(inputB, out int B);
             
+            Console.WriteLine("-----------------------------------------------------------------");
+            
             //Прямокутник
 
             for (int i = 0; i < A; i++)
@@ -31,6 +33,87 @@ namespace HW_1_6_2
                     Console.Write("*");
                 }
                 Console.WriteLine();
+            }
+            
+            Console.WriteLine("-----------------------------------------------------------------");
+            
+            //Прямокутний трикутник
+            
+            for (int i = 0; i < A; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("-----------------------------------------------------------------");
+            
+            //Рівносторонній трикутник
+            
+            int D = B;
+
+            int C = 1;
+            
+            for (int i = 0; i < A; i++)
+            {
+                for (int j = 0; j < B ; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < C; k++)
+                {
+                    Console.Write("*");
+                }
+
+                B -= 1;
+                C += 2;
+                Console.WriteLine();
+            }
+            Console.WriteLine("-----------------------------------------------------------------");
+            
+            //Ромб
+
+            C = 1;
+            
+            //int D = B;
+
+            for (int i = 0; i < (A + A + 1); i++)
+            {
+                if (i < A)
+                {
+                    for (int j = 0; j < D; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int k = 0; k < C; k++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    D -= 1;
+                    C += 2;
+                    Console.WriteLine();
+                }
+                else
+                {
+                    for (int j = 0; j < D; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int k = 0; k < C; k++)
+                    {
+                        Console.Write("*");
+                    }
+
+                    D += 1;
+                    C -= 2;
+                    Console.WriteLine();
+                }
             }
         }
     }
---
+++ HW_1_6_3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Enter number of client");
+var inputA = Console.ReadLine();
+int.TryParse(inputA, out int client);
+            
+Console.WriteLine("-----------------------------------------------------------------");
+
+int result = 1;
+do
+{
+    result *= client--;
+}
+while(client > 0);
+
+Console.WriteLine($"Number {result}");
+
